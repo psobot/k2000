2828f2bcac72d51dd863983e6aa429cdf81b543c
Peter Sobot <psobot@gmail.com>
2022-12-24 16:08:56 -0500

    Bump to v0.1.1

---
+++ k2000/__init__.py
@@ -0,0 +1,11 @@
+__author__ = "Peter Sobot"
+
+__version_tuple__ = (0, 1, 1)
+__version__ = ".".join([str(x) for x in __version_tuple__])
+
+__email__ = "github@petersobot.com"
+__description__ = (
+    "A library for working with the Kurzweil K2000/K2500/K2600 series of synthesizers."
+)
+__url__ = "https://github.com/psobot/k2000"
+__new_issue_url__ = "https://github.com/psobot/k2000/issues/new"
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 from os import path
+import k2000
 
 # io.open is needed for projects that support Python 2.7
 # It ensures open() defaults to text mode with universal newlines,
@@ -14,23 +15,24 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
+
+def find_meta(meta):
+    attr = getattr(k2000, "__{meta}__".format(meta=meta), None)
+    if not attr:
+        raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+    return attr
+
 
 setup(
     name="k2000",
-    version="1.0.0",
-    description="A library for working with the Kurzweil K2000/K2500/K2600 series of synthesizers.",
+    version=find_meta("version"),
+    description=find_meta("description"),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/psobot/k2000",
-    author="Peter Sobot",
-    author_email="github@petersobot.com",
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
+    author=find_meta("author"),
+    author_email=find_meta("email"),
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
@@ -40,17 +42,17 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
-    keywords="kurzweil midi synthesizer k2000 k2vx k2500 k2600",
+    keywords="kurzweil midi synthesizer k2000 k2vx k2500 k2600 sysex",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    install_requires=["tqdm", "Pillow", "python-rtmidi"],
+    install_requires=["Pillow", "python-rtmidi", "numpy"],
     extras_require={"dev": ["check-manifest"], "test": ["coverage"]},
     entry_points={
-        "console_scripts": [
-            "k2000=k2000.command_line:main",
-        ],
+        # "console_scripts": [
+        #     "k2000=k2000.command_line:main",
+        # ],
     },
     project_urls={
-        "Bug Reports": "https://github.com/psobot/k2000/issues",
-        "Source": "https://github.com/psobot/k2000/",
+        "Bug Reports": find_meta("new_issue_url"),
+        "Source": find_meta("url"),
     },
 )
